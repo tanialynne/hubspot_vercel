f3b0d7a09214ebf4852f19e7591a15ee1a843446
Tania <tania_lynne@yahoo.com>
2025-06-09 11:02:09 -0400

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,10 +1,13 @@
-const stripe = require('stripe')(process.env.STRIPE_STAGE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_STAGE_SECRET_KEY);  
 
 module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
 
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
   try {
