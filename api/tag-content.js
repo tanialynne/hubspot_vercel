ef328ac6336fe0316679acfdc20cafe462ec3a10
Tania <tania_lynne@yahoo.com>
2025-06-13 13:51:00 -0400

    Update tag-content.js

--- api/tag-content.js
+++ api/tag-content.js
@@ -1,15 +1,21 @@
 export default async function handler(req, res) {
-  // CORS + Preflight
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    return res.status(200).end();
+ const allowedOrigins = [
+    'https://www.heroic.us',
+    'https://45764384.hs-sites.com'
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight OPTIONS request immediately
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
