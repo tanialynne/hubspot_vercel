d332493413b3374848623abab4b9b5136a5036b4
Tania <tania_lynne@yahoo.com>
2025-06-13 13:40:35 -0400

    Update tag-content.js

--- api/tag-content.js
+++ api/tag-content.js
@@ -1,12 +1,20 @@
 export default async function handler(req, res) {
+  // CORS + Preflight
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(200).end();
+  }
+
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
-
+  
   const { email, tags } = req.body;
 
   const apiKey = process.env.ACTIVE_CAMPAIGN_API_KEY;
